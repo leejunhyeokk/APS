8a551015dad691cf43c9342f4fdca341d6bce9b0
LeeJunHyeok <dng20@naver.com>
2021-04-27 00:06:43 +0900

    [FIX] sw_1970

--- SW Expert Academy/D2/sw_1970.py
+++ SW Expert Academy/D2/sw_1970.py
@@ -1,11 +1,13 @@
 for t in range(1, int(input()) + 1):
     money = int(input())
     pay = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
-
     ans = [0] * 8
     for i in range(len(pay)):
-        if money != 0:
-            ans[i] = money // pay[i]
-            money = money % pay[i]
+        ans[i] = money // pay[i]
+        money = money % pay[i]
     print(f'#{t}')
-    print(f'{ans}')
+    for i in range(8):
+        if i == 7:
+            print(ans[i])
+        else:
+            print(ans[i], end=" ")
